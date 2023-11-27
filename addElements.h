5d61c20cdab95f075ec7a99d9e46fa7c865b6502
KwonMingi <74185795+KwonMingi@users.noreply.github.com>
2023-11-28 01:44:15 +0900

    Create addElements.h

---
+++ addElements.h
@@ -0,0 +1,100 @@
+#include <stdbool.h>
+/**
+ * 주어진 정수 값을 주어진 정수 포인터에 할당하고 포인터를 반환.
+ * @param valuePoint 정수 값을 할당할 정수형 포인터
+ * @param value 할당할 정수 값
+ * @return 할당된 정수 포인터
+ */
+int *addInt(int *valuePoint, int value)
+{
+    *valuePoint = value;
+    return valuePoint;
+}
+/**
+ * 주어진 long 값을 주어진 long 포인터에 할당하고 포인터를 반환.
+ * @param valuePoint long 값을 할당할 long형 포인터
+ * @param value 할당할 long 값
+ * @return 할당된 long 포인터
+ */
+long *addLong(long *valuePoint, long value)
+{
+    *valuePoint = value;
+    return valuePoint;
+}
+/**
+ * 주어진 long long 값을 주어진 long long 포인터에 할당하고 포인터를 반환.
+ * @param valuePoint long long 값을 할당할 long long형 포인터
+ * @param value 할당할 long long 값
+ * @return 할당된 long long 포인터
+ */
+long long *addLongLong(long long *valuePoint, long long value)
+{
+    *valuePoint = value;
+    return valuePoint;
+}
+/**
+ * 주어진 double 값을 주어진 double 포인터에 할당하고 포인터를 반환.
+ * @param valuePoint double 값을 할당할 double형 포인터
+ * @param value 할당할 double 값
+ * @return 할당된 double 포인터
+ */
+double *addDouble(double *valuePoint, double value)
+{
+    *valuePoint = value;
+    return valuePoint;
+}
+/**
+ * 주어진 long double 값을 주어진 long double 포인터에 할당하고 포인터를 반환.
+ * @param valuePoint long double 값을 할당할 long double형 포인터
+ * @param value 할당할 long double 값
+ * @return 할당된 long double 포인터
+ */
+long double *addLongDouble(long double *valuePoint, long double value)
+{
+    *valuePoint = value;
+    return valuePoint;
+}
+/**
+ * 주어진 short 값을 주어진 short 포인터에 할당하고 포인터를 반환.
+ * @param valuePoint short 값을 할당할 short형 포인터
+ * @param value 할당할 short 값
+ * @return 할당된 short 포인터
+ */
+short *addShort(short *valuePoint, short value)
+{
+    *valuePoint = value;
+    return valuePoint;
+}
+/**
+ * 주어진 char 값을 주어진 char 포인터에 할당하고 포인터를 반환.
+ * @param valuePoint char 값을 할당할 char형 포인터
+ * @param value 할당할 char 값
+ * @return 할당된 char 포인터
+ */
+char *addChar(char *valuePoint, char value)
+{
+    *valuePoint = value;
+    return valuePoint;
+}
+/**
+ * 주어진 bool 값을 주어진 bool 포인터에 할당하고 포인터를 반환.
+ * @param valuePoint bool 값을 할당할 bool형 포인터
+ * @param value 할당할 bool 값
+ * @return 할당된 bool 포인터
+ */
+bool *addBool(bool *valuePoint, bool value)
+{
+    *valuePoint = value;
+    return valuePoint;
+}
+/**
+ * 주어진 float 값을 주어진 float 포인터에 할당하고 포인터를 반환.
+ * @param valuePoint float 값을 할당할 float형 포인터
+ * @param value 할당할 float 값
+ * @return 할당된 float 포인터
+ */
+float *addFloat(float *valuePoint, float value)
+{
+    *valuePoint = value;
+    return valuePoint;
+}
